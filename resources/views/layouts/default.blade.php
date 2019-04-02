f984fb24b9f6676cb9aff13166a8b2a038023928
wlyywf <wlyywf@gmail.com>
2019-04-02 04:45:55 +0000

    init css

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,21 @@
 <html>
 <head>
   <title>@yield('title', 'weibo app')</title>
+  <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-@yield('content')
+  <nav class="navbar nvabar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="/">weibo app</a>
+      <ul class="navbar-nav justify-content-end">
+          <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+          <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+        </ul>
+    </div>
+  </nav>
+
+  <div class="container">
+    @yield('content')
+  </div>
 </body>
 </html>
