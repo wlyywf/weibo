41597c08e71519ac4125a9a35d2fc3ac5fbd9640
wlyywf <wlyywf@gmail.com>
2019-04-08 03:40:20 +0000

    weio model

--- app/Models/User.php
+++ app/Models/User.php
@@ -57,4 +57,9 @@ class User extends Authenticatable
             $user->activation_token = str_random(30);
         });
     }
+
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    }
 }
