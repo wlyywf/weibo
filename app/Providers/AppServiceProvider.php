8acaae4b96f729d55d935512d3dd32ed2938df95
wlyywf <wlyywf@gmail.com>
2019-04-09 09:07:59 +0000

    final commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,5 +26,20 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Carbon::setLocale('zh');
+        \DB::listen(function ($query) {
+            $tmp = str_replace('?', '"'.'%s'.'"', $query->sql);
+            $qBindings = [];
+            foreach ($query->bindings as $key => $value) {
+                if (is_numeric($key)) {
+                    $qBindings[] = $value;
+                } else {
+                    $tmp = str_replace(':'.$key, '"'.$value.'"', $tmp);
+                }
+            }
+            $tmp = vsprintf($tmp, $qBindings);
+            $tmp = str_replace("\\", "", $tmp);
+            \Log::info(' execution time: '.$query->time.'ms; '.$tmp."\n\n\t");
+
+        });
     }
 }
---
+++ resources/views/shared/_stats.blade.php
@@ -0,0 +1,18 @@
+<a href="{{ route('users.followings', $user->id) }}">
+  <strong id="following" class="stat">
+    {{ ($user->followings()->count()) }}
+  </strong>
+  关注
+</a>
+<a href="{{ route('users.followers', $user->id) }}">
+  <strong id="followers" class="stat">
+    {{ ($user->followers()->count()) }}
+  </strong>
+  粉丝
+</a>
+<a href="{{ route('users.show', $user->id) }}">
+  <strong id="statuses" class="stat">
+    {{ $user->statuses()->count() }}
+  </strong>
+  微博
+</a>
